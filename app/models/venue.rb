1f5915faafc6a675820c279bccd1ef2421ea463a
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-10-02 15:42:50 -0400

    formatted homepage and added weekly favorites:

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,12 +1,12 @@
 class Venue < ApplicationRecord
-	 mount_uploader :image, ImageUploader
-	 has_many :venue_categories
-	 has_many :categories, :through => :venue_categories
-	 has_many :ratings
-	 has_many :favorites
-	 has_many :posts
+	mount_uploader :image, ImageUploader
+	has_many :venue_categories
+	has_many :categories, :through => :venue_categories
+	has_many :ratings
+	has_many :favorites
+	has_many :posts
 
-	 def mean_rating
+	def mean_rating
     self.ratings.average(:rating).to_f
   end
   geocoded_by :address
